25f67b0b428aacca16c9a5ed4ce167094be98b78
DESKTOP-71FF4PU\gerip <geri.prenga@gmail.com>
2022-05-06 21:32:44 +0200

    chore: log cleanup

--- src/shared/getUsername.js
+++ src/shared/getUsername.js
@@ -3,7 +3,6 @@ export const getUsername = async (user) => {
     const response = await fetch('https://api.github.com/users/' + user);
     if (response.status === 200) {
         data = await response.json();
-        console.log(data);
     } else {
         data = null;
     }
