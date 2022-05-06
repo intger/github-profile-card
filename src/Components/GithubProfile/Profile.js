b6dd19f898ef1d08cce0b870b2d6207fe3ea4a83
DESKTOP-71FF4PU\gerip <geri.prenga@gmail.com>
2022-05-06 19:22:05 +0200

    feat: github profile component ( jsx + style )

--- src/Components/GithubProfile/Profile.js
+++ src/Components/GithubProfile/Profile.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useAppContext } from '../../context/AppContext';
+import { ReactComponent as BlogIcon } from '../../images/blog-solid.svg';
+import { ReactComponent as TwitterIcon } from '../../images/twitter-brands.svg';
+import { ReactComponent as Email } from '../../images/envelope-solid.svg';
 
 export const Profile = () => {
 
@@ -8,13 +11,43 @@ export const Profile = () => {
     console.log(githubData);
 
     return (
-        <section className='section github-profile'>
+        <section className='github-profile'>
             <div className='container'>
                 <h2>Profile for Github User - {githubData.login}</h2>
                 <div className='profile-card'>
+                    <div className='header'></div>
                     <div className='avatar'>
                         <img src={githubData.avatar_url} alt="Github Avatar" />
                     </div>
+                    <div className='info'>
+                        <span className='name'>{githubData.name}</span>
+                        <span className='bio'>{githubData.bio}</span>
+                    </div>
+                    <div className='follow-stats'>
+                        <div className='following'>
+                            <span className='number'>{githubData.following}</span>
+                            <span className='label'>Following</span>
+                        </div>
+                        <div className='followers'>
+                            <span className='number'>{githubData.followers}</span>
+                            <span className='label'>Followers</span>
+                        </div>
+                    </div>
+                    <div className='github-stats'></div>
+                    <button className='btn btn-primary github-btn' href={githubData.html_url}>Visit profile on Github</button>
+                    {githubData.blog || githubData.twitter_username ? 
+                    <div className='social'>
+                        {githubData.blog ?
+                            <a href={githubData.blog}  className='blog'><BlogIcon /></a>
+                         : null }
+                         {githubData.twitter_username ?
+                            <a href={`https://twitter.com/${githubData.blog}`} className='twitter'><TwitterIcon /></a>
+                        : null }
+                         {githubData.email ?
+                            <a href={`mailto:${githubData.email}`} className='email'><Email /></a>
+                        : null }
+                    </div> 
+                    : null}
                 </div>
             </div>
         </section>
